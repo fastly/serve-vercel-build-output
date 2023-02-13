b3a65a4fd85a9f4637283a07a013a1df88c75413
Katsuyuki Omuro <komuro@fastly.com>
2023-02-13 16:13:07 +0900

    export logging

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@
 
 /// <reference types='@fastly/js-compute' />
 
+export * from './logging/index';
+
 import VercelBuildOutputServer from './server/VercelBuildOutputServer';
 
 export default VercelBuildOutputServer;
---
+++ src/logging/index.ts
@@ -0,0 +1,11 @@
+import ILogger from "./ILogger";
+import ILoggerProvider from "./ILoggerProvider";
+import NullLogger from "./NullLogger";
+import ComputeJsConsoleLoggerProvider from "./ComputeJsConsoleLoggerProvider";
+
+export {
+  ILogger,
+  ILoggerProvider,
+  NullLogger,
+  ComputeJsConsoleLoggerProvider,
+};
