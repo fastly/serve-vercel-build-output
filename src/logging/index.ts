30aec480e1a1e141b5d364061af7869d4a46494f
Katsuyuki Omuro <komuro@fastly.com>
2023-02-13 16:15:10 +0900

    export logging

--- src/logging/index.ts
+++ src/logging/index.ts
@@ -1,11 +1,14 @@
 import ILogger from "./ILogger";
 import ILoggerProvider from "./ILoggerProvider";
 import NullLogger from "./NullLogger";
-import ComputeJsConsoleLoggerProvider from "./ComputeJsConsoleLoggerProvider";
+import ComputeJsConsoleLoggerProvider, { LogLevel, LoggerDef, LoggerDefs } from "./ComputeJsConsoleLoggerProvider";
 
 export {
   ILogger,
   ILoggerProvider,
   NullLogger,
   ComputeJsConsoleLoggerProvider,
+  LogLevel,
+  LoggerDef,
+  LoggerDefs,
 };
