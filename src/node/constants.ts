8c3bcf7c7640ff0c789e1f4ae7d7d372cae013c7
Katsuyuki Omuro <komuro@fastly.com>
2023-05-06 18:43:57 +0900

    Added webpack transform

--- src/node/constants.ts
+++ src/node/constants.ts
@@ -1,2 +1,3 @@
 export const SVBO_OPTIONS_KEY = '@fastly/serve-vercel-build-output';
 export const SVBO_ASSET_TRANSFORM_KEY = 'asset-transform';
+export const SVBO_WEBPACK_TRANSFORM_KEY = 'webpack-transform';
