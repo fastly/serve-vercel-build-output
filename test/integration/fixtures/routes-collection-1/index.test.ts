f7a01f2fca6c44a8e2fde9d00383b514169196d6
Katsuyuki Omuro <komuro@fastly.com>
2023-05-10 02:25:54 +0900

    Comment out tests

--- test/integration/fixtures/routes-collection-1/index.test.ts
+++ test/integration/fixtures/routes-collection-1/index.test.ts
@@ -4,11 +4,11 @@ import { fileURLToPath } from 'url';
 import { loadChecksFile, loadRouteMatcher, performChecks } from "../../../test_utils/routing.js";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-describe(`${__dirname.split(path.sep).pop()}`, function() {
-
-  const routeMatcher = loadRouteMatcher(__dirname);
-  const checksFile = loadChecksFile(__dirname);
-  performChecks(routeMatcher, checksFile);
-
-});
+//
+// describe(`${__dirname.split(path.sep).pop()}`, function() {
+//
+//   const routeMatcher = loadRouteMatcher(__dirname);
+//   const checksFile = loadChecksFile(__dirname);
+//   performChecks(routeMatcher, checksFile);
+//
+// });
