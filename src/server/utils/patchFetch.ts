51907b27750c713058606f2a19673aa503a242af
Katsuyuki Omuro <komuro@fastly.com>
2023-05-29 12:08:07 +0900

    Allow fetch hook handler to return null, to call original fetch

--- src/server/utils/patchFetch.ts
+++ src/server/utils/patchFetch.ts
@@ -7,13 +7,13 @@ export type OnBeforeFetchHandler = (
   fetch: typeof globalThis.fetch,
   input: Parameters<typeof globalThis.fetch>[0],
   init: Parameters<typeof globalThis.fetch>[1]
-) => ReturnType<typeof fetch>;
+) => ReturnType<typeof fetch> | null;
 
 export function onBeforeFetch(handler: OnBeforeFetchHandler) {
 
   const origFetch = fetch.bind(globalThis);
   globalThis.fetch = (input, init) => {
-    return handler(origFetch, input, init);
+    return handler(origFetch, input, init) ?? origFetch(input, init);
   };
 
 }
